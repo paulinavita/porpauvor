6432adb523d84502d26a27b024cdf5b9d022dcc2
paulinavita <paulinadavita@gmail.com>
2025-01-29 23:18:48 +0000

    feat: add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ const geistMono = Geist_Mono({
   subsets: ["latin"],
 });
 
-export const metadata = {
+export const metadata: Metadata = {
   title: "Porpauvor;",
   description: "paupau portfolio and some dev things",
   icons: {
